4eaf94c2de50e5bc5520d3aa9f5eb2ecadc8cece
Zach Pardee <zachpardee@gmail.com>
2019-04-26 13:26:23 -0600

    added some functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM has been fully loaded')
   console.table(gifts)
+
+  let giftList = document.querySelector(".gift-list");
+  let searchBar = document.querySelector("filter-imput");
+
+  for (let gift of gifts){
+
+    let giftListItem = document.createElement("li");
+    let giftListImg = document.createElement("img");
+    giftListImg.setAttribute("src", gift.image);
+    giftListItem.innerHTML = gift.name;
+
+    giftList.append(giftListItem);
+    giftList.append(giftListImg);
+  }
+
+
+    (function(){
+      ("#filter-input").on("keyup", function() {
+        var value = (this).val().toLowerCase();
+        ("gift-list li").filter(function() {
+          (this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    });
+
+
+
+
+
 })
